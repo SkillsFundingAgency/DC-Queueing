cd48ec28637d72977a7883295651eae3e7aa11a9
Sai Siddula <Sai.Siddula@gmail.com>
2018-05-17 14:58:14 +0100

    added Topic publish and Subscription services

---
+++ src/ESFA.DC.Queueing.Interface/ITopicPublishService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESFA.DC.Queueing.Interface
+{
+    public interface ITopicPublishService<in T>
+        where T : new()
+    {
+        Task PublishAsync(T obj);
+    }
+}
---
+++ src/ESFA.DC.Queueing.Interface/ITopicSubscriptionService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ESFA.DC.Queueing.Interface
+{
+    public interface ITopicSubscriptionService<T>
+    {
+        void Subscribe(Func<T, CancellationToken, Task<bool>> callback);
+
+        Task UnsubscribeAsync();
+    }
+}
